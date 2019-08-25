873898176e7b1c63379fbe686e38d97383cf868a
praveendxminds <47963837+praveendxminds@users.noreply.github.com>
2019-08-25 21:13:07 +0530

    Merge branch 'master' of https://github.com/praveendxminds/Ecommerce
    # Conflicts:
    #	app/src/main/java/com/app/ecommerce/Home/HomePage.java

--- app/src/main/java/com/nisarga/nisargaveggiez/Home/HomePage.java
+++ app/src/main/java/com/nisarga/nisargaveggiez/Home/HomePage.java
@@ -86,6 +86,7 @@ import com.mindorks.placeholderview.PlaceHolderView;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -121,6 +122,10 @@ public class HomePage extends AppCompatActivity implements NavigationView.OnNavi
 
         apiInterface = APIClient.getClient().create(APIInterface.class);
         session = new SessionManager(getApplicationContext());
+
+
+
+
 
         init();
         initApiCall();
