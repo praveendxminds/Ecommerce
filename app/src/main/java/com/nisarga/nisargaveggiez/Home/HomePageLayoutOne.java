9553dc7adcfa1b9fa13bf22be9372b6e2bffdbac
praveendxminds <47963837+praveendxminds@users.noreply.github.com>
2019-09-06 16:11:08 +0530

    Merge branch 'master' of https://github.com/praveendxminds/Ecommerce
    # Conflicts:
    #	app/src/main/java/com/app/ecommerce/Home/HomePage.java

---
+++ app/src/main/java/com/nisarga/nisargaveggiez/Home/HomePageLayoutOne.java
@@ -0,0 +1,50 @@
+package com.nisarga.nisargaveggiez.Home;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.LinearLayoutManager;
+import android.widget.TextView;
+
+import com.mindorks.placeholderview.PlaceHolderView;
+import com.mindorks.placeholderview.annotations.Click;
+import com.mindorks.placeholderview.annotations.Layout;
+import com.mindorks.placeholderview.annotations.NonReusable;
+import com.mindorks.placeholderview.annotations.Resolve;
+import com.mindorks.placeholderview.annotations.View;
+import com.nisarga.nisargaveggiez.R;
+import com.nisarga.nisargaveggiez.SessionManager;
+import com.nisarga.nisargaveggiez.retrofit.ProductslHomePage;
+
+import java.util.List;
+
+
+@NonReusable
+@Layout(R.layout.home_page_layout_one)
+public class HomePageLayoutOne {
+
+
+    @View(R.id.phvRecommendedList)
+    public PlaceHolderView phvRecommendedList;
+
+    Context mContext;
+    Object mList;
+    SessionManager session;
+
+    public HomePageLayoutOne(Context context, Object list) {
+        this.mContext = context;
+        this.mList = list;
+    }
+
+    @Resolve
+    public void onResolved()
+    {
+        phvRecommendedList.getBuilder()
+                .setHasFixedSize(false)
+                .setItemViewCacheSize(10)
+                .setLayoutManager(new LinearLayoutManager(mContext, LinearLayoutManager.HORIZONTAL, false));
+
+
+    }
+
+
+}
